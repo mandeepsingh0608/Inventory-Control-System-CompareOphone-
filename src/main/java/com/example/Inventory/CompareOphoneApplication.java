5819d7a8b18507224a9b1b3bea544ad4e73f714c
Mandeep <mandeepsingh0608@gmail.com>
2021-04-20 13:14:32 -0600

    changing main application name

--- src/main/java/com/example/Inventory/CompareOphoneApplication.java
+++ src/main/java/com/example/Inventory/CompareOphoneApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryApplication {
+public class CompareOphoneApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryApplication.class, args);
+		SpringApplication.run(CompareOphoneApplication.class, args);
 	}
 
 }
